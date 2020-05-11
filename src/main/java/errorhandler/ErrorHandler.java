3a2ecfb30bdf21f52c84471f1d61c1be7e0ac304
msi1 <r.sekaleshfar@gmail.com>
2020-05-11 22:01:31 +0430

    Alter ErrorHandler to a Utility class

--- src/main/java/errorhandler/ErrorHandler.java
+++ src/main/java/errorhandler/ErrorHandler.java
@@ -4,6 +4,8 @@ package errorhandler;
  * Created by Alireza on 6/28/2015.
  */
 public class ErrorHandler {
+    private ErrorHandler() {}
+
     public static boolean hasError = false;
 
     public static void printError(String msg) {
