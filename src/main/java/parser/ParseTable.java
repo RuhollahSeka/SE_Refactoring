02c1a50f68857caa30176938b2f5c35439d5e25a
msi1 <r.sekaleshfar@gmail.com>
2020-05-11 18:38:38 +0430

    Delete redundant lines from ParseTable

--- src/main/java/parser/ParseTable.java
+++ src/main/java/parser/ParseTable.java
@@ -1,9 +1,10 @@
 package parser;
 
 import scanner.token.Token;
-import scanner.type.Type;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by mohammad hosein on 6/25/2015.
@@ -11,11 +12,12 @@ import java.util.*;
 public class ParseTable {
     private ArrayList<Map<Token,Action>> actionTable;
     private ArrayList<Map<NonTerminal,Integer>> gotoTable;
+
     public ParseTable(String jsonTable) throws Exception {
         jsonTable = jsonTable.substring(2,jsonTable.length()-2);
-        String[] Rows = jsonTable.split("\\],\\[");
-        Map<Integer, Token> terminals = new HashMap<Integer, Token>();
-        Map<Integer,NonTerminal> nonTerminals = new HashMap<Integer, NonTerminal>();
+        String[] Rows = jsonTable.split("],\\[");
+        Map<Integer, Token> terminals = new HashMap<>();
+        Map<Integer,NonTerminal> nonTerminals = new HashMap<>();
         Rows[0] = Rows[0].substring(1,Rows[0].length()-1);
         String[] cols = Rows[0].split("\",\"");
         for (int i = 1; i < cols.length; i++) {
@@ -23,16 +25,16 @@ public class ParseTable {
                 String temp = cols[i].substring(5);
                 try {
                     nonTerminals.put(i, NonTerminal.valueOf(temp));
-                }catch (Exception e){
-                    temp = temp;
+                }catch (Exception ignored){
+
                 }
             }
             else {
                 terminals.put(i, new Token(Token.getTyepFormString(cols[i]), cols[i]));
             }
         }
-        actionTable = new ArrayList<Map<Token, Action>>();
-        gotoTable = new ArrayList<Map<NonTerminal, Integer>>();
+        actionTable = new ArrayList<>();
+        gotoTable = new ArrayList<>();
         for (int i = 1; i <Rows.length ; i++) {
             if(i == 100)
             {
@@ -41,7 +43,7 @@ public class ParseTable {
             }
             Rows[i] = Rows[i].substring(1,Rows[i].length()-1);
             cols = Rows[i].split("\",\"");
-            actionTable.add(new HashMap<Token, Action>());
+            actionTable.add(new HashMap<>());
             gotoTable.add(new HashMap<>());
             for (int j = 1; j <cols.length ; j++) {
                 if(!cols[j].equals(""))
@@ -51,13 +53,9 @@ public class ParseTable {
                     }else
                     if(terminals.containsKey(j))
                     {
-//                        try {
                         Token t = terminals.get(j);
                         Action a = new Action(cols[j].charAt(0) == 'r' ? act.reduce : act.shift, Integer.parseInt(cols[j].substring(1)));
                             actionTable.get(actionTable.size() - 1).put(t, a);
-//                        }catch (StringIndexOutOfBoundsException e){
-//                            e.printStackTrace();
-//                        }
                     }
                     else if(nonTerminals.containsKey(j)){
                         gotoTable.get(gotoTable.size()-1).put(nonTerminals.get(j),Integer.parseInt(cols[j]));
@@ -72,14 +70,9 @@ public class ParseTable {
 
     public int getGotoTable(int currentState, NonTerminal variable )
     {
-//        try {
-            return gotoTable.get(currentState).get(variable);
-//        }catch (NullPointerException dd)
-//        {
-//            dd.printStackTrace();
-//        }
-//        return 0;
+        return gotoTable.get(currentState).get(variable);
     }
+
     public Action getActionTable(int currentState ,Token terminal)
     {
         return actionTable.get(currentState).get(terminal);
